9224be2b43362d2c00a4a641a2dc2fee37e1a82e
Erich <erichroeseler123@gmail.com>
2026-01-14 10:22:45 -0700

    FIX: Changed class to className in layout for React compatibility

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import './globals.css';
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" class="bg-black">
-      <body class="bg-black antialiased overflow-hidden m-0 p-0">
+    <html lang="en" className="bg-black">
+      <body className="bg-black antialiased overflow-hidden m-0 p-0">
         {children}
       </body>
     </html>
