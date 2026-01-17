299ca8c6e16f84ce674fd2fffc048991a99dbc05
Erich <erichroeseler123@gmail.com>
2026-01-17 09:19:31 -0700

    feat: add Ford Amphitheater, Dillon Amphitheater, Mishawaka

---
+++ data/venues.ts
@@ -0,0 +1,26 @@
+  "ford-amphitheater": {
+    name: "Ford Amphitheater",
+    address: "95 Spectrum Loop, Colorado Springs, CO 80921",
+    capacity: "~8,000",
+    type: "Outdoor Amphitheatre",
+    seatgeekQuery: "Ford Amphitheater Colorado Springs",
+    shuttle: true,
+  },
+
+  "dillon-amphitheater": {
+    name: "Dillon Amphitheater",
+    address: "135 W Lodgepole St, Dillon, CO 80435",
+    capacity: "~3,500",
+    type: "Outdoor Amphitheatre",
+    seatgeekQuery: "Dillon Amphitheater",
+    shuttle: true,
+  },
+
+  "mishawaka": {
+    name: "Mishawaka Amphitheatre",
+    address: "13714 Poudre Canyon Rd, Bellvue, CO 80512",
+    capacity: "~800",
+    type: "Outdoor / Riverfront",
+    seatgeekQuery: "Mishawaka Amphitheatre",
+    shuttle: true,
+  },
