2deabc28ff90e7bf9dfa4768d8a2cfa8649edba2
Erich <erichroeseler123@gmail.com>
2026-01-17 20:13:01 -0700

    feat: populate venues registry with SeatGeek venue IDs

--- data/venues.ts
+++ data/venues.ts
@@ -1,36 +1,122 @@
-export const VENUES = {
+export const venues = {
+  "red-rocks-amphitheatre": {
+    "name": "Red Rocks Amphitheatre",
+    "city": "Morrison, CO",
+    "seatgeekId": 196
+  },
   "mission-ballroom": {
-    name: "Mission Ballroom",
-    city: "Denver",
-    state: "CO",
-    seatgeekId: 651,
+    "name": "Mission Ballroom",
+    "city": "Denver, CO",
+    "seatgeekId": 467142
   },
-
   "ogden-theatre": {
-    name: "Ogden Theatre",
-    city: "Denver",
-    state: "CO",
-    seatgeekId: 244,
+    "name": "Ogden Theatre",
+    "city": "Denver, CO",
+    "seatgeekId": 849
   },
-
-  "ford-amphitheater": {
-    name: "Ford Amphitheater",
-    city: "Colorado Springs",
-    state: "CO",
-    seatgeekId: 8892,
+  "fillmore-auditorium-denver": {
+    "name": "Fillmore Auditorium",
+    "city": "Denver, CO",
+    "seatgeekId": 451
   },
-
+  "bluebird-theater-denver": {
+    "name": "Bluebird Theater",
+    "city": "Denver, CO",
+    "seatgeekId": 555
+  },
+  "gothic-theatre": {
+    "name": "Gothic Theatre",
+    "city": "Englewood, CO",
+    "seatgeekId": 828
+  },
+  "paramount-theatre-co": {
+    "name": "Paramount Theatre",
+    "city": "Denver, CO",
+    "seatgeekId": 717
+  },
+  "the-oriental-theater": {
+    "name": "Oriental Theater",
+    "city": "Denver, CO",
+    "seatgeekId": 7435
+  },
+  "fiddler-s-green-amphitheatre": {
+    "name": "Fiddler’s Green Amphitheatre",
+    "city": "Greenwood Village, CO",
+    "seatgeekId": 3703
+  },
+  "ball-arena": {
+    "name": "Ball Arena",
+    "city": "Denver, CO",
+    "seatgeekId": 187
+  },
+  "empower-field-at-mile-high": {
+    "name": "Empower Field at Mile High",
+    "city": "Denver, CO",
+    "seatgeekId": 1131
+  },
+  "denver-coliseum": {
+    "name": "Denver Coliseum",
+    "city": "Denver, CO",
+    "seatgeekId": 3081
+  },
+  "cervantes-masterpiece": {
+    "name": "Cervantes’ Masterpiece Ballroom",
+    "city": "Denver, CO",
+    "seatgeekId": 1537
+  },
+  "temple-denver": {
+    "name": "Temple",
+    "city": "Denver, CO",
+    "seatgeekId": 435021
+  },
+  "the-church-nightclub-denver": {
+    "name": "Church Nightclub",
+    "city": "Denver, CO",
+    "seatgeekId": 8905
+  },
+  "meow-wolf-denver": {
+    "name": "Meow Wolf",
+    "city": "Denver, CO",
+    "seatgeekId": 519590
+  },
+  "hq-denver": {
+    "name": "HQ",
+    "city": "Denver, CO",
+    "seatgeekId": 1608167
+  },
+  "larimer-lounge": {
+    "name": "Larimer Lounge",
+    "city": "Denver, CO",
+    "seatgeekId": 5508
+  },
+  "globe-hall-denver": {
+    "name": "Globe Hall",
+    "city": "Denver, CO",
+    "seatgeekId": 172339
+  },
+  "fox-theatre-boulder": {
+    "name": "Fox Theatre",
+    "city": "Boulder, CO",
+    "seatgeekId": 1261
+  },
+  "boulder-theater": {
+    "name": "Boulder Theater",
+    "city": "Boulder, CO",
+    "seatgeekId": 1031
+  },
+  "ford-amphitheater-colorado-springs": {
+    "name": "Ford Amphitheater",
+    "city": "Colorado Springs, CO",
+    "seatgeekId": 522235
+  },
   "dillon-amphitheater": {
-    name: "Dillon Amphitheater",
-    city: "Dillon",
-    state: "CO",
-    seatgeekId: 1589,
+    "name": "Dillon Amphitheater",
+    "city": "Dillon, CO",
+    "seatgeekId": 50043
   },
-
   "mishawaka-amphitheatre": {
-    name: "Mishawaka Amphitheatre",
-    city: "Bellvue",
-    state: "CO",
-    seatgeekId: 324,
-  },
+    "name": "Mishawaka Amphitheatre",
+    "city": "Bellvue, CO",
+    "seatgeekId": 6122
+  }
 } as const;
