7a2661de0858179c46fe16fce9bcd51d4b9e0db1
Erich <erichroeseler123@gmail.com>
2026-01-19 11:48:16 -0700

    lock: Red Rocks 2026 homepage schedule + round-trip pricing

--- app/page.tsx
+++ app/page.tsx
@@ -15,19 +15,76 @@ import {
 ========================= */
 
 const UPCOMING_SHOWS = [
-  { id: 2, artist: "Crankdat w/ Dr. Fresch", date: "MAR 27, 2026", time: "7:00 PM" },
-  { id: 3, artist: "Ravenscoon & Jantsen", date: "MAR 28, 2026", time: "6:00 PM" },
-  { id: 4, artist: "INZO", date: "APR 03, 2026", time: "6:00 PM" },
-  { id: 5, artist: "It's Murph presents Murph Rocks", date: "APR 04, 2026", time: "8:00 PM" },
-  { id: 6, artist: "Zingara & Level Up", date: "APR 10, 2026", time: "6:00 PM" },
-  { id: 7, artist: "Liquid Stranger", date: "APR 11, 2026", time: "6:00 PM" },
-  { id: 8, artist: "John Mulaney: Mister Whatever", date: "APR 15, 2026", time: "7:30 PM" },
-  { id: 9, artist: "bbno$ w/ Oliver Tree", date: "APR 16, 2026", time: "6:30 PM" },
-  { id: 10, artist: "Sublime", date: "APR 17, 2026", time: "6:30 PM" },
-  { id: 11, artist: "Sublime", date: "APR 18, 2026", time: "6:30 PM" },
-  { id: 12, artist: "Wiz Khalifa w/ 2 Chainz", date: "APR 19, 2026", time: "5:30 PM" },
-  { id: 13, artist: "Ice Cube", date: "APR 20, 2026", time: "7:00 PM" },
-  { id: 14, artist: "Ethel Cain", date: "APR 21, 2026", time: "7:00 PM" }
+  { id: 1, artist: "Two Friends", date: "MAY 01, 2026", time: "7:00 PM" },
+  { id: 2, artist: "Jason Isbell and the 400 Unit", date: "MAY 02, 2026", time: "7:00 PM" },
+  { id: 3, artist: "Puscifer", date: "MAY 03, 2026", time: "7:30 PM" },
+  { id: 4, artist: "Bright Eyes", date: "MAY 06, 2026", time: "7:00 PM" },
+  { id: 5, artist: "Alejandro Fernández", date: "MAY 07, 2026", time: "8:00 PM" },
+  { id: 6, artist: "Cloonee", date: "MAY 09, 2026", time: "7:00 PM" },
+  { id: 7, artist: "Hippie Sabotage", date: "MAY 10, 2026", time: "7:00 PM" },
+  { id: 8, artist: "YUNGBLUD – IDOLS World Tour", date: "MAY 11, 2026", time: "8:00 PM" },
+  { id: 9, artist: "Russell Dickerson", date: "MAY 13, 2026", time: "7:00 PM" },
+  { id: 10, artist: "LIGHTCODE by LSDREAM", date: "MAY 16, 2026", time: "9:30 AM" },
+  { id: 11, artist: "LSDREAM presents: DREAMROCKS II", date: "MAY 16, 2026", time: "6:00 PM" },
+  { id: 12, artist: "The Elovaters", date: "MAY 17, 2026", time: "5:00 PM" },
+  { id: 13, artist: "Khalid", date: "MAY 18, 2026", time: "7:30 PM" },
+  { id: 14, artist: "Kevin Gates", date: "MAY 19, 2026", time: "7:00 PM" },
+  { id: 15, artist: "flipturn", date: "MAY 21, 2026", time: "6:00 PM" },
+  { id: 16, artist: "Seven Lions", date: "MAY 22, 2026", time: "6:00 PM" },
+  { id: 17, artist: "FISHER", date: "MAY 23, 2026", time: "7:00 PM" },
+  { id: 18, artist: "Alabama Shakes", date: "MAY 24, 2026", time: "7:00 PM" },
+  { id: 19, artist: "Alabama Shakes", date: "MAY 25, 2026", time: "7:00 PM" },
+  { id: 20, artist: "Michael Franti & Spearhead", date: "MAY 29, 2026", time: "7:00 PM" },
+  { id: 21, artist: "Alan Walker", date: "MAY 30, 2026", time: "6:00 PM" },
+
+  { id: 22, artist: "Alex Warren", date: "JUN 02, 2026", time: "7:30 PM" },
+  { id: 23, artist: "Brit Floyd – The Wall", date: "JUN 04, 2026", time: "8:00 PM" },
+  { id: 24, artist: "Brit Floyd – Dark Side of the Moon", date: "JUN 05, 2026", time: "8:00 PM" },
+  { id: 25, artist: "Big Head Todd and the Monsters", date: "JUN 06, 2026", time: "7:00 PM" },
+  { id: 26, artist: "Lord Huron", date: "JUN 10, 2026", time: "7:30 PM" },
+  { id: 27, artist: "Trevor Hall", date: "JUN 14, 2026", time: "5:30 PM" },
+  { id: 28, artist: "Rod Stewart", date: "JUN 15, 2026", time: "7:30 PM" },
+  { id: 29, artist: "Rod Stewart", date: "JUN 16, 2026", time: "7:30 PM" },
+  { id: 30, artist: "Amyl and The Sniffers", date: "JUN 17, 2026", time: "8:00 PM" },
+  { id: 31, artist: "THIRD DAY – 30th Anniversary", date: "JUN 18, 2026", time: "7:00 PM" },
+  { id: 32, artist: "Louis Tomlinson", date: "JUN 19, 2026", time: "7:00 PM" },
+  { id: 33, artist: "O.A.R.", date: "JUN 20, 2026", time: "6:30 PM" },
+  { id: 34, artist: "Weird Al Yankovic", date: "JUN 23, 2026", time: "7:30 PM" },
+
+  { id: 35, artist: "Treaty Oak Revival", date: "JUL 01, 2026", time: "7:00 PM" },
+  { id: 36, artist: "DEADROCKS XII", date: "JUL 02, 2026", time: "4:30 PM" },
+  { id: 37, artist: "DEADROCKS XII", date: "JUL 03, 2026", time: "5:00 PM" },
+  { id: 38, artist: "The Avett Brothers", date: "JUL 10, 2026", time: "7:30 PM" },
+  { id: 39, artist: "The Avett Brothers", date: "JUL 11, 2026", time: "7:30 PM" },
+  { id: 40, artist: "The Avett Brothers", date: "JUL 12, 2026", time: "6:30 PM" },
+  { id: 41, artist: "KALEO", date: "JUL 14, 2026", time: "7:30 PM" },
+  { id: 42, artist: "The Head And The Heart", date: "JUL 15, 2026", time: "7:30 PM" },
+  { id: 43, artist: "The Head And The Heart", date: "JUL 16, 2026", time: "7:30 PM" },
+  { id: 44, artist: "The String Cheese Incident", date: "JUL 17, 2026", time: "6:00 PM" },
+  { id: 45, artist: "The String Cheese Incident", date: "JUL 18, 2026", time: "7:00 PM" },
+
+  { id: 46, artist: "Parker McCollum", date: "JUL 29, 2026", time: "7:30 PM" },
+  { id: 47, artist: "Killer Queen", date: "JUL 30, 2026", time: "8:00 PM" },
+  { id: 48, artist: "Mt. Joy", date: "AUG 13, 2026", time: "7:00 PM" },
+  { id: 49, artist: "Mt. Joy", date: "AUG 14, 2026", time: "7:00 PM" },
+  { id: 50, artist: "Train", date: "AUG 17, 2026", time: "6:45 PM" },
+  { id: 51, artist: "Joe Bonamassa", date: "AUG 23, 2026", time: "7:00 PM" },
+  { id: 52, artist: "Ray LaMontagne", date: "AUG 26, 2026", time: "7:00 PM" },
+
+  { id: 53, artist: "Maná", date: "SEP 04, 2026", time: "8:30 PM" },
+  { id: 54, artist: "Maná", date: "SEP 05, 2026", time: "8:30 PM" },
+  { id: 55, artist: "Gregory Alan Isakov", date: "SEP 06, 2026", time: "8:00 PM" },
+  { id: 56, artist: "Gregory Alan Isakov", date: "SEP 07, 2026", time: "8:00 PM" },
+  { id: 57, artist: "Five Finger Death Punch", date: "SEP 08, 2026", time: "6:45 PM" },
+  { id: 58, artist: "Get The Led Out", date: "SEP 17, 2026", time: "7:30 PM" },
+
+  { id: 59, artist: "Matt Rife", date: "OCT 18, 2026", time: "7:00 PM" },
+  { id: 60, artist: "Mersiv", date: "OCT 23, 2026", time: "5:00 PM" },
+  { id: 61, artist: "Evanescence", date: "OCT 26, 2026", time: "7:00 PM" },
+  { id: 62, artist: "Cypress Hill · Method Man · Redman", date: "OCT 29, 2026", time: "6:00 PM" },
+
+  { id: 63, artist: "mike.", date: "NOV 14, 2026", time: "5:30 PM" },
+  { id: 64, artist: "mike.", date: "NOV 15, 2026", time: "2:30 PM" },
 ];
 
 /* =========================
@@ -83,7 +140,7 @@ export default function ShuttleHomePage() {
           </h2>
 
           <p className="mt-2 text-xl font-black text-blue-600">
-            $50 per person · $250 minimum
+            $50 per person · $250 minimum - Round Trip Price
           </p>
 
           <p className="mt-6 text-lg text-slate-700">
